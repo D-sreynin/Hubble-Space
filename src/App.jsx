3d778df4422c3fba8c164f78fce83f204bbcb7f5
D-sreynin <sreynindoeun@gmail.com>
2025-01-15 18:59:18 +0700

    Add component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-// App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './assets/components/Navbar';
 import LandingPage from './assets/components/LandingPage';
-import FeatureNews from './assets/components/FeaturedNews';
 import Science from './assets/components/Science';
-import VideoHighlight from './assets/components/VideoHighlight';
-import EarthInfoCenter from './assets/components/EarthInfoCenter';
-import ImageOfTheDay from './assets/components/ImageOfTheDay';
-import ImageNasa from './assets/components/ImageNasa';
+import Apod from './assets/components/scienceComponent/Apod';
+import SpaceXmission from './assets/components/scienceComponent/SpaceXmission';
+import MissionSummarize from './assets/components/scienceComponent/MissionSummarize'; // Added
+import PeopleOfNasa from './assets/components/scienceComponent/PeopleOfNasa';
+import Skywatching from './assets/components/scienceComponent/Skywatching';
+import ApodDetail from './assets/components/scienceComponent/ApodDetail';
+import Footer from './assets/components/Footer';
 import './index.css';
-
+import FeatureNews from './assets/components/FeaturedNews';
 function App() {
   return (
-    <div>
-      <Navbar />
-      <LandingPage /> 
-      <FeatureNews />
-      <Science />
-      <VideoHighlight />
-      <EarthInfoCenter />
-      <ImageOfTheDay />
-      <ImageNasa />
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/science" element={<Science />} />
+          <Route path="/science/apod" element={<Apod />} />
+          <Route path="/science/apod-detail" element={<ApodDetail />} />
+          <Route path="/science/spacex-mission" element={<SpaceXmission />} />
+          <Route path="/science/mission-summarize/:id" element={<MissionSummarize />} /> {/* Dynamic Route */}
+          <Route path="/science/people-of-nasa" element={<PeopleOfNasa />} />
+          <Route path="/science/skywatching" element={<Skywatching />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
